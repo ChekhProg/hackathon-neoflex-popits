907f90869a9062ba7f6599c7fa4d78988cd4baeb
Chekh <chekhprogrammer@gmail.com>
2021-07-03 17:06:51 +0400

    first working

--- build.sbt
+++ build.sbt
@@ -8,7 +8,12 @@ lazy val `candles` =
     .service("candles")
     .settings(
       libraryDependencies ++= Dependencies.`akka-streams`,
-      libraryDependencies += "com.typesafe.akka" %% "akka-stream" % AkkaVersion
+      libraryDependencies += "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+      libraryDependencies += "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
+      libraryDependencies += "com.typesafe.akka" %% "akka-stream-typed" % AkkaVersion,
+      libraryDependencies += "net.debasishg" %% "redisclient" % "3.30",
+      libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.2.11",
+      libraryDependencies ++= Seq("com.typesafe.play" % "play-json_2.11" % "2.4.2")
     )
     .dependsOn(
       `domain-common`,
--- service/candles/app/src/main/scala/rht/hack/Main.scala
+++ service/candles/app/src/main/scala/rht/hack/Main.scala
@@ -1,5 +1,16 @@
 package rht.hack
 
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.{Logging, LoggingAdapter}
+
+import rht.common.domain.candles.{Candle, CandleDetails, common}
+
+import scala.concurrent.duration.FiniteDuration
+import com.redis.{M, PubSubMessage, RedisClient, S, U}
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+
 /**
   * Program entry point
   *
@@ -16,7 +27,38 @@ object Main extends HackathonApp {
     */
   override def start(args: List[String]): SourceActor = {
     // TODO: Implement this method to start your app
-    null
+
+    def write(det: CandleDetails) = {
+      val obj = JObject(List(
+        "low" -> JDecimal(det.low),
+        "high" -> JDecimal(det.high),
+        "open" -> JDecimal(det.open),
+        "close" -> JDecimal(det.close),
+      ))
+      val doc = render(obj)
+      val json = pretty(doc)
+      json
+    }
+
+    val r = new RedisClient("localhost", 6379)
+
+    class MyActor(r: RedisClient) extends Actor {
+      val log: LoggingAdapter = Logging(context.system, this)
+
+      def receive: Receive = {
+        case x @ Candle(interval: FiniteDuration, figi: common.Figi, details: CandleDetails) =>
+          val json = write(x.details)
+          r.set(figi.value, json)
+          log.info("candle: " + figi.value + "\n" + json)
+        case x      => log.info("received unknown message: " + x.getClass)
+      }
+    }
+
+    val system: ActorSystem = ActorSystem("ActorGuide")
+
+    val pinger = system.actorOf(Props(classOf[MyActor], r), "pinger")
+
+    pinger
   }
 
 }
